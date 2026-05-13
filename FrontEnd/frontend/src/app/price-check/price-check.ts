6cc2ee91fe6b5d3373c0c63284a677b223f4f721
RVXTD <njg44@njit.edu>
2026-05-13 11:32:22 -0400

    Components added

---
+++ FrontEnd/frontend/src/app/price-check/price-check.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-check',
+  imports: [],
+  templateUrl: './price-check.html',
+  styleUrl: './price-check.css',
+})
+export class PriceCheck {}
